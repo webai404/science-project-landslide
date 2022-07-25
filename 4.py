804c4099af39396c317dab6450f2490d9136f64c
一群科展學生 <102276810+webai404@users.noreply.github.com>
2022-07-25 11:15:49 +0800

    Update 4.py

--- 4.py
+++ 4.py
@@ -2,7 +2,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
 import time
-import urllib.request 
+import urllib.request
 import numpy as np
 import os.path as path
 import os
@@ -10,14 +10,15 @@ import cv2 as cv
 import argparse
 import RPi.GPIO as GPIO
 import gmail
-#import ard
+import ard
 import line
-#os.system('./ffmpeg.sh &')
-#設定攝影機
+
+# os.system('./ffmpeg.sh &')
+# 設定攝影機
 camera = PiCamera()
 camera.resolution = (64, 64)
 rawCapture = PiRGBArray(camera, size=(64, 64))
-#新視窗命名為Faces
+# 新視窗命名為Faces
 display_window = cv2.namedWindow('Faces')
 parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
                                               OpenCV. You can process both videos and images.')
@@ -30,50 +31,50 @@ else:
     backSub = cv.createBackgroundSubtractorKNN()
 camera.framerate = 15
 camera.rotation = 180
-k=0
-x=0
+k = 0
+x = 0
 water = False
 light = False
 img_my = False
 r = 3
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    k=k+1
+    k = k + 1
     camera.start_preview()
     img = frame.array
     fgMask = backSub.apply(img)
-    fgMask=cv2.medianBlur(fgMask,3)
+    fgMask = cv2.medianBlur(fgMask, 3)
     for i in range(63):
-        i = i+1
-        for j in range (63):
-            j= j+1
-            a = fgMask[1*i,1*j]
-            b = fgMask[1*i-1,1*j]
-            
-            c = fgMask[1*i,1*j-1]
-            d = fgMask[1*i-1,1*j-1]
-            #print(a,',',b,',',c,',',d,',')
-            f = a+b+c+d
-            #print(f)
-            #print(f,i,j)
-            if((f==252 and k>3) and (img_my == False)):
+        i = i + 1
+        for j in range(63):
+            j = j + 1
+            a = fgMask[1 * i, 1 * j]
+            b = fgMask[1 * i - 1, 1 * j]
+
+            c = fgMask[1 * i, 1 * j - 1]
+            d = fgMask[1 * i - 1, 1 * j - 1]
+            # print(a,',',b,',',c,',',d,',')
+            f = a + b + c + d
+            # print(f)
+            # print(f,i,j)
+            if ((f == 252 and k > 3) and (img_my == False)):
                 print("img")
                 gmail.send(2)
                 line.send(2)
                 img_my = True
                 r += 1
                 break
-            
-        if(f==252):
+
+        if (f == 252):
             break
     cv2.imshow('Faces', fgMask)
     cv2.imshow('img', img)
     key = cv2.waitKey(1)
 
     rawCapture.truncate(0)
-    #x = ard.check()
-    
-    '''if (x == 1 and not water):
+    x = ard.check()
+
+    if (x == 1 and not water):
         print("water")
         gmail.send(0)
         line.send(0)
@@ -87,9 +88,12 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         r += 1
     else:
         print("nope")
-    '''
+
     if r == 3:
         break
-    if key == ord("q"): # Press 'q' to quit
-        
+    if key == ord("q"):  # Press 'q' to quit
+
         break
+
+    if not ard.check_rain():
+        time.sleep(1)
